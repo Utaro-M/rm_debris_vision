9fecc45431395609675ceffe4b36e8b3cb8b56dd
Utaro=M <utaro.matsuura@gmail.com>
2020-11-20 00:43:21 +0900

    get plane で3トピックサブスクライブするようにした

--- src/get_plane.py
+++ src/get_plane.py
@@ -9,24 +9,12 @@ from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PolygonStamped
 from jsk_recognition_msgs.msg import PolygonArray
 from jsk_recognition_msgs.msg import ClusterPointIndices
-# def timer_cb(msg):
-#     info_msg. =  msg.transforms.header.stamp
-#     pub_info.publish(info_msg)
-
-
-# if __name__ == '__main__':
-#     rospy.init_node('static_virtual_camera')
-
-#     pub_info = rospy.Publisher('~camera_inf', CameraInfo, queue_size=1)
-#     info_msg = CameraInfo()
-#     info_msg.header.frame_id = 'static_virtual_camera'
-    
-#     rospy.Subscriber("multi_plane_extraction/output",PointCloud2 , timer_cb)        
-#     # rospy.Timer(rospy.Duration(1.0/30), timer_cb)
-#     rospy.spin()nppn
+from jsk_recognition_msgs.msg import ModelCoefficientsArray
 thre = 10000
-def timer_cb(msg1,msg2):
+def timer_cb(msg1,msg2,msg3):
     # print(msg1.header)
+    info_msg_polygon=msg2
+    info_msg_coefficient=msg3
     planes_indices = msg1.cluster_indices
     ind=0
     longest_ind=0
@@ -34,31 +22,29 @@ def timer_cb(msg1,msg2):
     if(len(planes_indices) >=1):
         for plane in planes_indices:
             length=len(plane.indices)
-            # print ("loop{}".format(ind))
-            # print(length)
             if (length>longest_length):
                 longest_ind=ind
                 longest_length=length
                 ind+=1
-                # info_msg.header=msg1.header
-        # print(msg2.polygons[longest_ind].polygon.points[0])
-        info_msg=msg2.polygons[longest_ind]
-        pub_info.publish(info_msg)
-
+                
+        info_msg_polygon.polygons=[msg2.polygons[longest_ind]]
+        info_msg_coefficient.coefficients=[msg3.coefficients[longest_ind]]
+        pub_info_polygon.publish(info_msg_polygon)
+        pub_info_coefficient.publish(info_msg_coefficient)
+        pub_info_polygonstamped.publish(msg2.polygons[longest_ind])
 if __name__ == '__main__':
     rospy.init_node('get_plane')
 
-    pub_info = rospy.Publisher('~output', PolygonStamped, queue_size=1)
-    info_msg=PolygonStamped()
-    sub1 = message_filters.Subscriber("/organized_multi_plane_segmentation/output_refined",ClusterPointIndices)
-    sub2 = message_filters.Subscriber("/organized_multi_plane_segmentation/output_refined_polygon",PolygonArray )
-    # sub1 = message_filters.Subscriber("/multi_plane_sac_segmentation/output_refined",ClusterPointIndices)
-    # sub2 = message_filters.Subscriber("/multi_plane_sac_segmentation/output_refined_polygon",PolygonArray )
-    
+    pub_info_polygon = rospy.Publisher('~output_polygon', PolygonArray, queue_size=1)
+    pub_info_coefficient = rospy.Publisher('~output_coefficient', ModelCoefficientsArray, queue_size=1)
+    pub_info_polygonstamped = rospy.Publisher('~focus_plane', PolygonStamped, queue_size=1)
+    sub1 = message_filters.Subscriber("plane_extraction/plane_cluster_indices",ClusterPointIndices)
+    sub2 = message_filters.Subscriber("plane_extraction/plane_polygons",PolygonArray )
+    sub3 = message_filters.Subscriber("plane_extraction/plane_coefficients", ModelCoefficientsArray)
     fps=10.0
     delay=1 / fps *0.5
 
-    mf= message_filters.ApproximateTimeSynchronizer([sub1,sub2],5,delay)
+    mf= message_filters.ApproximateTimeSynchronizer([sub1,sub2,sub3],5,delay)
     mf.registerCallback(timer_cb)
 
     rospy.spin()
