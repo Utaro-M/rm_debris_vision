84d6190ce66650a879ae75ca170c30e111f19ed1
Utaro=M <utaro.matsuura@gmail.com>
2021-05-08 14:22:55 +0900

    候補点表示、選択、IKができた

--- scripts/put_points_on_image.py
+++ scripts/put_points_on_image.py
@@ -2,19 +2,21 @@
 
 import rospy
 import numpy as np
-# import message_filters
+import message_filters
 import tf2_ros
 import tf2_geometry_msgs
 from cv_bridge import CvBridge
 import cv2
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import Joy
 from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import PolygonStamped
 from geometry_msgs.msg import Polygon #test
 from geometry_msgs.msg import PoseStamped
 from image_geometry import PinholeCameraModel
-from rm_debris_vision.srv import GraspCandidates 
+from rm_debris_vision.srv import GraspCandidates
+from rm_debris_vision.srv import TriggerWithData
 
 class PutPointsOnImage(object):
     def __init__(self):
@@ -26,12 +28,15 @@ class PutPointsOnImage(object):
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         self.bridge = CvBridge()
 
+        self.state = "sleep"
         self.flag=False
         self.u = 0
         self.v = 0
         self.u_v_list = []
         self.header_frame_id=""
-
+        self.hands_interval=[100,300,500]
+        self.cursor=0
+        
         # publish
         self.pub_point_stamped = rospy.Publisher("~output", PointStamped, queue_size=1)
         self.pub_image = rospy.Publisher("~output/image", Image, queue_size=1)
@@ -39,6 +44,12 @@ class PutPointsOnImage(object):
         # subscribe
         rospy.Subscriber('~input/camera_info', CameraInfo, self.camera_info_cb)
         rospy.Subscriber("~input/image", Image, self.image_cb)
+        #Joy
+        sub1 = message_filters.Subscriber('~vive_left', Joy)
+        sub2 = message_filters.Subscriber('~vive_right', Joy)
+        self.mf= message_filters.ApproximateTimeSynchronizer([sub1,sub2],2,10)
+        self.mf.registerCallback(self.joy_cb)
+
         self.s=rospy.Service("display_grasp_candidates", GraspCandidates,self.display) #use service
         # rospy.Subscriber("~input/polygon_stamped", PolygonStamped, self.polygon_stamped_cb) #subscribe points
         # rospy.Subscriber("~input/point_stamped", PointStamped, self.point_stamped_cb)
@@ -48,10 +59,42 @@ class PutPointsOnImage(object):
         self.frame_id = msg.header.frame_id
         self.is_camera_arrived = True
 
+    # Trigger, Trackpad X, Trackpad Y
+    # axes: [0.0, 0.0, 0.0]
+    # Trigger, Trackpad touched, Trackpad pressed, Menu, Gripper
+    # buttons: [0, 0, 0, 0, 0]
+    def joy_cb(self,left_msg,right_msg):
+        if (self.state == "sleep"):
+            if left_msg.buttons[4]==1 and right_msg.buttons[4]==1:
+                self.send_trigger(True,self.hands_interval)
+                self.state = "waiting"
+        elif(self.state == "waiting"):
+            if left_msg.buttons[3]==1:
+                self.cursor +=1
+            elif right_msg.buttons[3]==1:
+                self.cursor -=1
+            if(self.cursor < 0):
+                self.cursor = 0
+                # self.cursor = len(self.u_v_list)/2 -1
+            if(len(self.u_v_list)/2 <= self.cursor):
+                self.cursor = len(self.u_v_list)/2 -1
+                # self.cursor = 0
+            if left_msg.buttons[0]==1 and right_msg.buttons[0]==1:
+                self.send_trigger(False,[self.cursor])
+                self.state = "choosed"
+
+    def send_trigger(self,flag,data):
+        rospy.wait_for_service('joy_trigger')
+        try:
+            Trigger_with_data = rospy.ServiceProxy('joy_trigger', TriggerWithData)
+            resp1 = Trigger_with_data(flag,data)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+
     # add circles
     def image_cb(self,msg):
         self.header_frame_id = msg.header.frame_id
-        if self.flag :
+        if self.state == "waiting" :
             print "in image_cb"
             try:
                 img = self.bridge.imgmsg_to_cv2(msg,msg.encoding)
@@ -63,7 +106,11 @@ class PutPointsOnImage(object):
                     color=(0,0,255)
                 else:
                     color=(255,0,0)
-                img_circle = cv2.circle(img,(int(self.u_v_list[i][0]),int(self.u_v_list[i][1])),10,color,-1)
+                if (i == self.cursor or i == self.cursor + len(self.u_v_list)/2):
+                    size=16
+                else:
+                    size=10
+                img_circle = cv2.circle(img,(int(self.u_v_list[i][0]),int(self.u_v_list[i][1])),size,color,-1)
                 img_msg = self.bridge.cv2_to_imgmsg(img,msg.encoding)
                 self.pub_image.publish(img_msg)
         else:
